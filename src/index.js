9bc949d7a943d2a71720fc98db4f5d4c8b2bc9c2
kang.chen <kang.chen@coupang.com>
2018-11-28 16:05:36 +0800

    update config

--- src/index.js
+++ src/index.js
@@ -4,15 +4,12 @@ const express = require("express");
 const proxy = require("http-proxy-middleware");
 const compression = require("compression");
 const chalk = require("chalk");
+const fs = require("fs");
 
 const app = express();
 const middlewares = require("./middlewares");
 
-const targetProxy = process.env.targetProxy;
-const port = process.env.port;
-const host = process.env.host;
-const rewritePath = process.env.rewritePath;
-const oldPath = process.env.oldPath;
+const { targetProxy, port, host, rewritePath, oldPath, config } = process.env;
 
 // enable cors request
 app.use(
@@ -25,16 +22,24 @@ app.use(compression());
 
 middlewares(app);
 
-app.use(
-  "/*",
-  proxy({
-    target: targetProxy,
-    changeOrigin: true,
-    pathRewrite: {
-      ["^" + oldPath]: rewritePath
-    }
-  })
-);
+if (config) {
+  const { localServer, proxyServer } = JSON.parse(
+    fs.readFileSync(`${process.cwd()}/proxy.json`)
+  );
+
+  console.log(localServer);
+} else {
+  app.use(
+    "/*",
+    proxy({
+      target: targetProxy,
+      changeOrigin: true,
+      pathRewrite: {
+        ["^" + oldPath]: rewritePath
+      }
+    })
+  );
+}
 
 const server = http.createServer(app);
 
