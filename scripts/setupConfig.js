b2409ec0ce0d00dcf0962f4e9e6f1fccc8d670d7
kang.chen <kang.chen@coupang.com>
2018-12-07 14:42:22 +0800

    change template path 2

--- scripts/setupConfig.js
+++ scripts/setupConfig.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const chalk = require("chalk");
 
-const configTemplate = fs.readFileSync(`${process.cwd()}/template/proxy.json`);
+const configTemplate = fs.readFileSync(
+  `${process.cwd()}/scripts/template/proxy.json`
+);
 
 fs.writeFileSync(`${process.cwd()}/proxy.json`, configTemplate);
 
