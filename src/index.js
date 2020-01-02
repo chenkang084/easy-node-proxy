085f8d88ab8548f750087533367b4794c3d28f04
kang.chen <kang.chen@coupang.com>
2020-01-02 14:03:09 +0800

    add allow orgin

--- src/index.js
+++ src/index.js
@@ -1,41 +1,50 @@
-const http = require("http");
-const cors = require("cors");
-const express = require("express");
-const proxy = require("http-proxy-middleware");
-const compression = require("compression");
-const chalk = require("chalk");
-const fs = require("fs");
-const moment = require("moment");
+const http = require('http');
+const cors = require('cors');
+const express = require('express');
+const proxy = require('http-proxy-middleware');
+const compression = require('compression');
+const chalk = require('chalk');
+const fs = require('fs');
+const moment = require('moment');
 
 const app = express();
-const middlewares = require("./middlewares");
+const middlewares = require('./middlewares');
 
 const { targetProxy, rewritePath, oldPath, config } = process.env;
 let { port, host } = process.env;
 
-// enable cors request
-app.use(
-  cors({
-    credentials: true
-  })
-);
+// function relayRequestHeaders(proxyReq, req) {
+//   console.log(req.headers.cookie);
+//   Object.keys(req.headers).forEach(function(key) {
+//     proxyReq.setHeader(key, req.headers[key]);
+//   });
+// }
 
 app.use(compression());
 
 // middlewares(app);
 
-if (config) {
+// if (config) {
+if (true) {
   const proxyPath = `${process.cwd()}/proxy.json`;
   if (!fs.existsSync(proxyPath)) {
-    console.log(
-      chalk.red(
-        `proxy.json doesn't exist, pls execuate 'npm run generate:config'`
-      )
-    );
+    console.log(chalk.red(`proxy.json doesn't exist, pls execuate 'npm run generate:config'`));
     return;
   }
 
-  const { localServer, proxyServer } = JSON.parse(fs.readFileSync(proxyPath));
+  const { localServer, proxyServer, allowOrigin = [] } = JSON.parse(fs.readFileSync(proxyPath));
+
+  function relayResponseHeaders(proxyRes, req, res) {
+    Object.keys(proxyRes.headers).forEach(function(key) {
+      res.append(key, proxyRes.headers[key]);
+    });
+
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+    if (allowOrigin.includes(req.headers.origin)) {
+      res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    }
+  }
 
   if (localServer) {
     localServer.port && (port = localServer.port);
@@ -49,41 +58,39 @@ if (config) {
         proxy({
           target,
           changeOrigin: true,
-          logLevel: "debug",
-          logProvider: provider => {
+          logLevel: 'debug',
+          logProvider: (provider) => {
             return {
               ...provider,
-              debug: log => {
-                console.log(moment().format("YYYY-MM-DD HH:mm:ss") + ":" + log);
+              debug: (log) => {
+                console.log(moment().format('YYYY-MM-DD HH:mm:ss') + ':' + log);
               },
-              info: log => {
+              info: (log) => {
                 console.log(
-                  chalk.green(
-                    `${moment().format(
-                      "YYYY-MM-DD HH:mm:ss"
-                    )} :[HPM] Proxy created:${path}  ->  ${target}`
-                  )
+                  chalk.green(`${moment().format('YYYY-MM-DD HH:mm:ss')} :[HPM] Proxy created:${path}  ->  ${target}`)
                 );
               }
             };
           },
-          ...opts
+          ...opts,
+          // onProxyReq: relayRequestHeaders,
+          onProxyRes: relayResponseHeaders
         })
       );
     });
   } else {
-    chalk.red("proxyServer must be specified!");
+    chalk.red('proxyServer must be specified!');
     process.exit();
   }
 } else {
   app.use(
-    "/*",
+    '/*',
     proxy({
       target: targetProxy,
       changeOrigin: true,
-      logLevel: "debug",
+      logLevel: 'debug',
       pathRewrite: {
-        ["^" + oldPath]: rewritePath
+        ['^' + oldPath]: rewritePath
       }
     })
   );
@@ -92,15 +99,11 @@ if (config) {
 const server = http.createServer(app);
 
 // Start your app.
-server.listen(port, host, err => {
+server.listen(port, host, (err) => {
   if (err) {
     console.log(err);
   }
 
-  console.log(chalk.green("node proxy start successfully!"));
-  console.log(
-    chalk.green(
-      `The node server can proxy http://${host + ":" + port}/api/* --> /api/*`
-    )
-  );
+  console.log(chalk.green('node proxy start successfully!'));
+  console.log(chalk.green(`The node server can proxy http://${host + ':' + port}/api/* --> /api/*`));
 });
